6c72540a43c4b4ee078c6506b4363f87ce669d4c
Jewell <jewellsmailbox@gmail.com>
2018-04-05 21:34:54 -0700

    initial commit

---
+++ spinwords.js
@@ -0,0 +1,15 @@
+
+function spinWords(string){
+ 
+  var stringSplit = string.split(' ')
+  for(var i = 0; i > stringSplit.length; i++) {
+  var testWord = stringSplit[i]
+  if(testWord.length > 3) {
+  var newWord = testword.split('').reverse().join('');
+}
+}
+var newWords = stringSplit.join(" ")
+
+}
+
+spinWords("hi my name is jewell")
